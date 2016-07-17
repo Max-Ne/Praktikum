35675ce7cb6ed49d677110d4f52d9dc2b6e433f0
Oskar Taubert <oskar-t@gmx.de>
2016-07-18 00:10:07 +0200

    hall plateaus

--- Quanten_Hall/QHE.py
+++ Quanten_Hall/QHE.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 #####################################
 #
 # Filename : QHE.py
@@ -9,14 +9,16 @@
 #
 # Creation Date : Sa 16 Jul 2016 19:14:31 CEST
 #
-# Last Modified : So 17 Jul 2016 18:25:38 CEST
+# Last Modified : Mo 18 Jul 2016 00:08:16 CEST
 #
 #####################################
 
 import numpy as np
-import matplotlib.pyplot as plt
+from ROOT import *
 
-#setup
+#init
+gROOT.Reset()
+
 T = 0. #K
 I = 0. #muA
 
@@ -31,6 +33,17 @@ Uread = False
 plotname = ""
 
 
+
+Bss = []
+Uss = []
+
+Is = []
+Ts = []
+Udirections = []
+
+gs = []
+filenames = []
+
 with open("data/index.txt", 'r') as indexf:
     for line in indexf:
         if (Bread and Uread):
@@ -38,16 +51,30 @@ with open("data/index.txt", 'r') as indexf:
             Bs = Bs * 6. / Bs.max()
             Us = np.array(Us, dtype=float)
 
+            Bss.append(Bs)
+            Uss.append(Us)
+            Ts.append(T)
+            Is.append(I)
+            Udirections.append(plotname[:4])
+            
+            g = TGraph(len(Bs), Bs, Us)
 
-            plt.plot(Bs, Us, 'ro')
-            plt.xlabel("B / T")
-            plt.ylabel(plotname[:4] + " / " + U_unit)
-            plt.tight_layout()
+            filename = "plots/" + plotname + ".pdf"
+            filenames.append(filename)
+
+            g.SetTitle(plotname[:4] + " mit T = " + str(T) + " K und I = " + str(I) + "muA;B / T;" + plotname[:4] + " / " + U_unit)
+            gs.append(g)
 
 
-
-            plt.savefig("plots/" + plotname + ".pdf")
-            plt.show()
+#            plt.plot(Bs, Us, 'ro')
+#            plt.xlabel("B / T")
+#            plt.ylabel(plotname[:4] + " / " + U_unit)
+#            plt.tight_layout()
+#
+#
+#
+#            plt.savefig("plots/" + plotname + ".pdf")
+#            plt.show()
 
             Bread = False
             Uread = False
@@ -127,4 +154,52 @@ with open("data/index.txt", 'r') as indexf:
                     dline = dataf.readline()
                 Uread = True
 
+
+
+#fitting
+print(Ts)
+print(Is)
+print(Udirections)
+
+
+fit_minss = [[2., 4.],[1.6, 3.], [2.2, 4.], [1.6, 3.2], [1.4, 2.3, 3.7], [2.1, 4.], [2.2, 4.2]]
+
+fit_maxss = [[2.7, 5.3], [2.1, 3.7], [2.4, 5.2], [2.05, 3.7], [1.6, 2.7, 5.5], [2.3, 5.3], [2.7, 4.6]]
+
+initvals = [[0.13, 0.27], [0.025, 0.05], [0.7, 1.3], [0.11, 0.2], [0.1, 0.14, 0.26], [0.65, 1.3]]
+
+for i,g in enumerate(gs):
+
+    if(Ts[i] < 3. and Is[i] == 20.):
+        nfits = 3
+    else:
+        nfits = 2
+
+    fs = []
+    if Udirections[i] == "U_26":
+        fitstr = "[0]*exp(-0.5*((x-[1])/[2])^2) + [3]"
+    elif Udirections[i] == "U_34":
+        fitstr = "[0]"
+       
+    for j in range(nfits):
+        fs.append(TF1("Fit" + str(i+1), fitstr, fit_minss[i][j], fit_maxss[i][j]))
+#    for j in range(nfits):
+        if Udirections[i] == "U_26":
+            fs[j].SetParameters(0.01, (fit_minss[i][j] + fit_maxss[i][j])/2, 0.5, 0.1)
+        elif Udirections[i] == "U_34":
+            fs[j].SetParameter(0, initvals[i][j])
+
+#    for j in range(nfits):
+        gs[i].Fit("Fit" + str(i+1), 'R')
+
+    c1 = TCanvas("c1", "", 200, 10, 700, 500 )
+    c1.SetGrid()
+    c1.cd()
+    g.Draw("AP")
+    for f in fs:
+        f.Draw("SAME")
+
+    c1.Update()
+
+    c1.SaveAs(filenames[i])
 
